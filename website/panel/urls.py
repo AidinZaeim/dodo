7418f43ebf6eab22dc2f07d1dae8da67c25cc342
Taha <TahaM000@gmail.com>
2023-09-03 18:28:29 +0330

    create panel app && create management page

---
+++ website/panel/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import *
+
+
+app_name = "panel"
+urlpatterns = [
+    path('management/',Management.as_view(),name='management'),
+
+]
--- website/website/settings.py
+++ website/website/settings.py
@@ -20,6 +20,7 @@ INSTALLED_APPS = [
 INSTALLED_APPS += [
     'home',
     'accounts',
+    'panel',
 ]
 
 MIDDLEWARE = [
