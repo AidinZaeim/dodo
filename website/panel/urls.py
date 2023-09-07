a7af4a424e2fd788d081797c5bff87c0e0501e47
TahaM8000 <TahaM000@gmail.com>
2023-09-07 17:46:20 +0330

    conflict

--- website/panel/urls.py
+++ website/panel/urls.py
@@ -5,4 +5,5 @@ from .views import *
 app_name = "panel"
 urlpatterns = [
     path('management/',Management.as_view(),name='management'),
+    path('ProjectDetail/<int:id>/',ProjectDetail.as_view(),name='ProjectDetail'),
 ]
--- website/panel/views.py
+++ website/panel/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views import View
+from .models import Project
 # Create your views here.
 
 
@@ -7,4 +8,21 @@ class Management(View):
 
     def get(self,request):
         return render(request,"panel/management.html")
+    
+
+class ProjectDetail(View):
+
+    def get(self,request,id):
+        projects_obj = Project.objects.get(id=id)
+
+        # tasks = projects_obj.tasks.all().order_by("deadline")
+
+
+        Info = {
+            'project' : projects_obj,
+            # 'tasks' : tasks,
+        }
+
+
+        return render(request,"panel/projectDetail.html",Info) 
     
