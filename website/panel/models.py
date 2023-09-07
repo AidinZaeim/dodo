d58b58603502c08c856f9a910445fb8d60c7e432
AidinZaeim <aidinzaeim@gmail.com>
2023-09-07 16:59:55 +0330

    section and task model - project model update

--- website/panel/models.py
+++ website/panel/models.py
@@ -1,17 +1,23 @@
 from django.db import models
-from accounts.models import *
+from accounts.models import CustomUser
 
 
 class Project(models.Model):
     name = models.CharField(max_length=100)
     owner = models.ForeignKey(CustomUser,null=True,blank=True,related_name="projects",on_delete=models.CASCADE)
-    members= models.ManyToManyField(CustomUser,related_name="member_of_project")
-
-
+    members = models.ManyToManyField(CustomUser,related_name="member_of_project")
 
     def __str__(self):
         return self.name
 
+
+class Section(models.Model):
+    project = models.ForeignKey(Project, related_name="sections",on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    creator = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
 
 
 class Task(models.Model):
@@ -23,17 +29,14 @@ class Task(models.Model):
     )
     creator = models.ForeignKey(CustomUser,null=True,blank=True,related_name="craeted_tasks",on_delete=models.CASCADE)
     under_taker = models.ForeignKey(CustomUser,null=True,blank=True,related_name="tasks",on_delete=models.CASCADE)
-    project = models.ForeignKey(Project,null=True,blank=True,on_delete=models.CASCADE)
     label = models.CharField(max_length=100)
     check_box = models.BooleanField(default=False)
     priority = models.CharField(max_length=1,choices=priority_choices)
     description = models.CharField(max_length=500)
     deadline = models.DateTimeField()
-    # duration
+    secion = models.ForeignKey(Section, related_name="tasks", on_delete=models.CASCADE)
+
 
 
     def __str__(self):
         return self.label
-
-
-
